46a1c6e7a28bbb0c1d05245986f52ef2946f87b5
Stan-is-the-man <stanislav.yanakievv@gmail.com>
2023-02-15 11:01:17 +0200

    test

--- history.py
+++ history.py
@@ -2,3 +2,31 @@ a = [1, 2, 3, 4, 5, 6, 7]
 b = [num for num in a if num % 2 == 0]
 print(b)
 
+word = ' I love SQL'
+print(word[:: -1])
+
+text = 'I love SQL'
+word = text.split(' ')
+rev = []
+
+for _ in range(len(word)):
+    rev.append(word.pop())
+
+print(' '.join(rev))
+
+
+from collections import deque
+
+people = deque()
+
+while True:
+    command = input()
+    if command == 'End':
+        print(people)
+        break
+    elif command == 'Paid':
+        while len(people):
+            print(f'Customer {people.popleft()} has been paid')
+    else:
+        people.append(command)
+
--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-a = 5
+from collections import deque
+
+people = deque()
+
+while True:
+    command = input()
+    if command == 'End':
+        print(people)
+        break
+    elif command == 'Paid':
+        while len(people):
+            print(f'Customer {people.popleft()} has been paid')
+    else:
+        people.append(command)
